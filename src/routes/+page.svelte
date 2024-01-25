39664d3db9976268b7c42b7087e23e0515de4b27
Jon Shipman <jon@jonshipman.com>
2024-01-24 18:04:56 -0600

    cookie border

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,14 @@
+<script lang="ts">
+	import cookies from './cookies.webp';
+</script>
+
 <svelte:head>
 	<title>ElizaShipman.com</title>
 </svelte:head>
 
-<iframe
-	src="/proxy"
-	title="Cookies"
-	class="absolute inset-0 border-0 h-screen w-screen"
+<iframe src="/proxy" title="Cookies" class="absolute inset-0 border-0 h-screen w-screen" />
+
+<div
+	class="pointer-events-none absolute bottom-0 w-screen pb-32 bg-bottom bg-repeat-x"
+	style="background-image: url('{cookies}');"
 />

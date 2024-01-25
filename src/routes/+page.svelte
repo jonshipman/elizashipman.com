f627eb7594e0987884a7a1171a01059145d99270
Jon Shipman <jon@jonshipman.com>
2024-01-24 18:17:29 -0600

    meta content

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,20 @@
 <script lang="ts">
+	import { page } from '$app/stores';
 	import cookies from './cookies.webp';
+
+	$: url = $page.url.origin + $page.url.pathname;
 </script>
 
 <svelte:head>
 	<title>ElizaShipman.com</title>
+	<meta property="og:url" content={url} />
+	<meta property="og:type" content="website" />
+	<meta property="og:title" content="Eliza Shipman: Cookie Baroness" />
+	<meta
+		property="og:description"
+		content="Fuel your adventures with Girl Scout Cookies! From Thin Mints to Tagalongs, we've got the perfect treats to keep you going all season long. Order online and get ready to sell!"
+	/>
+	<meta property="og:image" content="{url}favicon.ico" />
 </svelte:head>
 
 <iframe src="/proxy" title="Cookies" class="absolute inset-0 border-0 h-screen w-screen" />
